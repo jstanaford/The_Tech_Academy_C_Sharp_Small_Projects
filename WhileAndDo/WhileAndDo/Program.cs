b4fc39a61b0c2231ffe2938e2f93777445b6bee2
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-03 18:07:09 -0800

    WhileAndDo Upload

---
+++ WhileAndDo/WhileAndDo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileAndDo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Guess a number? ");
+			Console.WriteLine("Hint: the number is between 1 and 20");
+			int number = Convert.ToInt32(Console.ReadLine());
+			bool isGuessed = number == 13;
+
+			do
+			{
+				switch (number)
+				{
+					case 13:
+						Console.WriteLine("You guessed the number 13! That is correct!");
+						isGuessed = true;
+						break;
+					default:
+						Console.WriteLine("Sorry, that is wrong.");
+						Console.WriteLine("Guess again?");
+						number = Convert.ToInt32(Console.ReadLine());
+						break;
+
+				}
+			}
+			while (!isGuessed);
+
+
+			Console.ReadLine();
+		}
+	}
+}
