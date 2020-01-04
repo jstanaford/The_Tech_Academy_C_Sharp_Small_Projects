cd6b649c8c474b3d3de48abcce1aaa2d80b27c26
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-04 12:25:23 -0800

    WhileAnd Do update no 2

--- WhileAndDo/WhileAndDo/Program.cs
+++ WhileAndDo/WhileAndDo/Program.cs
@@ -10,34 +10,47 @@ namespace WhileAndDo
 	{
 		static void Main(string[] args)
 		{
-
-			Console.WriteLine("Guess a number? ");
-			Console.WriteLine("Hint: the number is between 1 and 20");
-			int number = Convert.ToInt32(Console.ReadLine());
-			bool isGuessed = number == 13;
-
-			do
-			{
-				switch (number)
 				{
-					case 13:
-						Console.WriteLine("You guessed the number 13! That is correct!");
-						Console.WriteLine("Thank you for playing! Have a good day!");
-						Console.WriteLine("Press esc to exit");
-						isGuessed = true;
-						break;
-					default:
-						Console.WriteLine("Sorry, that is wrong.");
-						Console.WriteLine("Guess again?");
-						number = Convert.ToInt32(Console.ReadLine());
-						break;
 
+				Console.WriteLine("Guess a number? ");
+				Console.WriteLine("Hint: the number is between 1 and 20");
+				Console.WriteLine("In case you were curious, here are the numbers one through 20:");
+				{
+					int i = 0;
+					while (i < 21)
+					{
+						Console.WriteLine(i);
+						i++;
+					}
 				}
+				int number = Convert.ToInt32(Console.ReadLine());
+				bool isGuessed = number == 13;
+					
+				do
+				{
+					switch (number)
+					{
+						case 13:
+							Console.WriteLine("You guessed the number 13! That is correct!");
+							Console.WriteLine("Thank you for playing! Have a good day!");
+							Console.WriteLine("Press enter to exit");
+							isGuessed = true;
+							break;
+						default:
+							Console.WriteLine("Sorry, that is wrong.");
+							Console.WriteLine("Guess again?");
+							number = Convert.ToInt32(Console.ReadLine());
+							break;
+
+						}
+					}
+				while (!isGuessed);
+
+
+				Console.ReadLine();
+				
+
 			}
-			while (!isGuessed);
-
-
-			Console.ReadLine();
 		}
 	}
 }
