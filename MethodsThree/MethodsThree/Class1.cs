af55d7a45faf13a50f8b5ea33b70738efb074b33
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-07 19:22:59 -0800

    Methodsthree upload 1

---
+++ MethodsThree/MethodsThree/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsThree
+{
+	public class Class1
+	{
+		public int MyMath { get; set; }
+
+
+
+
+		public int MyMethod1(int num)
+		{
+			int Answer1 = num + 100;
+			return (Answer1);
+		}
+
+		public int MyMethod2(int num)
+		{
+			int Answer2 = num - 100;
+			return (Answer2);
+		}
+
+		public int MyMethod3(int num)
+		{
+			int Answer3 = num * 100;
+			return (Answer3);
+		}
+
+
+
+
+	}
+
+
+}
---
+++ MethodsThree/MethodsThree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Class1 constant = new Class1();
+			constant.MyMath = 100;
+
+
+			Console.WriteLine("Hello, User!\nThis is my exercise for step 111 of the Tech Academy.\n");
+			Console.WriteLine("In this program, I will ask you for a number.\nThen I will add, subtract, and multiply that number by 100.");
+
+
+
+			Console.WriteLine("Please enter a number you would like added by 100 then press enter.");
+			int userInput = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("\nCalculating your number...\n\n");
+			userInput = constant.MyMethod1(userInput);
+			Console.WriteLine("Your number is: " + userInput);
+			Console.ReadLine();
+
+
+
+			Console.WriteLine("Please enter a number you would like subtracted by 100 then press enter.");
+			int userInput2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("\nCalculating your number...\n\n");
+			userInput2 = constant.MyMethod2(userInput2);
+			Console.WriteLine("Your number is: " + userInput2);
+			Console.ReadLine();
+
+			Console.WriteLine("Please enter a number you would like multiplied by 100 then press enter.");
+			int userInput3 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("\nCalculating your number...\n\n");
+			userInput3 = constant.MyMethod3(userInput3);
+			Console.WriteLine("Your number is: " + userInput3);
+			Console.ReadLine();
+
+			Console.WriteLine("\n\nThank you for playing! Press enter to close.");
+			Console.WriteLine();
+		}
+	}
+}
