b66b98d641d44c9b174dfc956a600b7a3c5f7b5f
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-08 13:32:25 -0800

    VoidMethod Upload

---
+++ VoidMethodPractice/VoidMethodPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodPractice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		
+			Console.WriteLine("Hello, welcome to this program. Press enter to see something.");
+			Console.ReadLine();
+
+			MyMath constant = new MyMath();
+			constant.VoidMethod(12, 73);
+			constant.VoidMethod(numTwo: 12, numOne: 73);
+			Console.ReadLine();
+
+		}
+
+		
+	}
+
+	public class MyMath
+	{
+		public int MyProperty { get; set; }
+
+
+		public void VoidMethod(int numOne, int numTwo)
+		{
+			numOne = numOne * numOne;
+			Console.WriteLine(numTwo);
+
+		}
+	}
+}
