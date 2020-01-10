2fe1d13d6a9984486a65d3d87cce490b2ed8ff30
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-10 12:26:10 -0800

    Practice with Struct

---
+++ StructPractice/StructPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPractice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Number price1 = new Number();
+			price1.Amount = 105.50m;
+
+
+			Console.WriteLine(price1.Amount);
+			Console.ReadLine();
+		}
+	}
+	
+	public struct Number
+	{
+		public decimal Amount;
+	}
+}
