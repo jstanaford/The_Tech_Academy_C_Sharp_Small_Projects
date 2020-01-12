c1b7e6d272e3208d383e98b972f463ce14192bce
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-11 17:22:00 -0800

    Constructor Practice

---
+++ ConstructorChain/ConstructorChain/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChain
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string userName = "Jacob";
+
+			var newPerson = new Person("Jacobeus");
+		}
+	}
+
+	public class Person
+	{
+		public string last;
+		public string first;
+
+		public Person(string lastName) : this(lastName, "Fred")
+		{
+			
+		}
+
+		public Person(string lastName, string firstName)
+		{
+			last = lastName;
+			first = firstName;
+			last = "Jakey";
+			first = "Staneeeford";
+		}
+	}
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
@@ -11,9 +10,10 @@ namespace TwentyOne
 	{
 		static void Main(string[] args)
 		{
-			
+			const string casinoName = "Grand Hotel and Casino";
 
-			Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+		
+			Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
 			string playerName = Console.ReadLine();
 			Console.WriteLine("And how much money did you bring today?");
 			int bank = Convert.ToInt32(Console.ReadLine());
@@ -22,6 +22,12 @@ namespace TwentyOne
 			if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya" )
 			{
 				Player player = new Player(playerName, bank);
+				player.Id = Guid.NewGuid();
+				using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\log.txt", true))
+				{
+					file.WriteLine(player.Id);
+					
+				}
 				Game game = new TwentyOneGame();
 				game += player;
 				player.isActivelyPlaying = true;
@@ -33,7 +39,7 @@ namespace TwentyOne
 				Console.WriteLine("Thank you for playing!");
 
 			}
-			Console.WriteLine("Feel free to look around the casino. Byefor now.");
+			Console.WriteLine("Feel free to look around the casino. Bye for now.");
 			Console.Read();
 
 
