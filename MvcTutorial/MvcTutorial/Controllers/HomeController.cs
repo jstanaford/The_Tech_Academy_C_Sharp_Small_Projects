4c303ba8a32066569e4d286223c7ca08bafe9d92
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-15 16:01:23 -0800

    General update 7

---
+++ MvcTutorial/MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MvcTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			User user = new User();
+			user.Id = 1;
+			user.FirstName = "Jake";
+			user.LastName = "Stanaford";
+			user.Age = 22;
+			return View(user);
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			throw new Exception("Invalid page");
+		}
+
+		public ActionResult Contact(int id=0)
+		{
+			ViewBag.Message = id;
+
+			return View();
+		}
+	}
+}
