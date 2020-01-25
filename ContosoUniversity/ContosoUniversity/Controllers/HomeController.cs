389d47b85ac2b7a1c0f7a9676fcf327e476a681a
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-01-25 13:00:44 -0800

    Contoso University Tutorial update

--- ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
 	public class HomeController : Controller
 	{
+		private SchoolContext db = new SchoolContext();
 		public ActionResult Index()
 		{
 			return View();
@@ -15,9 +18,21 @@ namespace ContosoUniversity.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			IQueryable<EnrollmentDateGroup> data = from students in db.Students
+												   group students by students.EnrollmentDate into dateGroup
+												   select new EnrollmentDateGroup()
+												   {
+													   EnrollmentDate = dateGroup.Key,
+													   StudentCount = dateGroup.Count()
+												   };
 
-			return View();
+			return View(data.ToList());
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
 		}
 
 		public ActionResult Contact()
